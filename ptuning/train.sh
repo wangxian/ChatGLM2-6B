32bcef8fab5d99a59deb52e503656f268043277a
wangxian <xian366@126.com>
2023-07-12 19:36:23 +0800

    12

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -19,10 +19,10 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
-    --max_steps 3000 \
+    --max_steps 300 \
     --logging_steps 10 \
-    --save_steps 1000 \
+    --save_steps 100 \
     --learning_rate $LR \
-    --pre_seq_len $PRE_SEQ_LEN \
+    --pre_seq_len $PRE_SEQ_LEN
 
 # --quantization_bit 4
